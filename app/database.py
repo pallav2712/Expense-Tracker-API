32c436d86ece22a10de0ed3a9a5e2dabda93d5e7
pallav2712 <pallavsharma1994@gmail.com>
2026-05-06 23:45:18 +0530

    made expense schema through psycopg2

--- app/database.py
+++ app/database.py
@@ -2,11 +2,13 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 
+from .config import settings
 
 
 try:
-    conn = psycopg2.connect(host='localhost', database='ExpenseTrakerAPI', user='postgres',
-                                password='051727', cursor_factory=RealDictCursor)
+    conn = psycopg2.connect(host=settings.database_hostname, database=settings.database_name, 
+                            user=settings.database_username, password=settings.database_password,
+                            cursor_factory=RealDictCursor)
     cursor = conn.cursor()
     print("Database connection was sucessfull!")
 
@@ -14,3 +16,21 @@ except Exception as error:
     print("Connecting to database failed")
     print("Error: ", error)   
     time.sleep(2)
+
+
+
+#expense schema
+expense_table = '''
+    CREATE TABLE IF NOT EXISTS expense (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    item VARCHAR NOT NULL,
+    quantity INT NOT NULL,
+    price INT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+'''
+
+cursor.execute(expense_table)
+conn.commit()
+
+print("Expense table created successfully!")
